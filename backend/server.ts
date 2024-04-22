a47ccdfe7595b317d81facf3a07dfbce17c94d48
S Soumyakanta <ssoumyakanta@fightright.in>
2024-04-22 21:45:28 +0530

    prettirer and firtst route in express

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import app from "./src/app";
+
+const stasrtServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`listening on ${port}`);
+  });
+};
+
+stasrtServer();
